445c6772797d8a15caea336dd6fc98df7cf08c90
Sander Lange <swipeytwit@gmail.com>
2026-04-18 17:34:59 +0200

    Update camera index and image save path
    Changed camera index from 1 to 0 and updated image save path.

--- src/pc/camera.py
+++ src/pc/camera.py
@@ -1,11 +1,13 @@
 import cv2 as cv
 import os
 import time
+from ImageSplitter import create_matrix
 
 allocatedTime = 1
 startTime = time.time()
+path = "DIN PATH HER, BOZO"
 
-camera = cv.VideoCapture(1)
+camera = cv.VideoCapture(0)
 
 res, frame = camera.read()
 count = 0
@@ -19,10 +21,12 @@ while camera.isOpened():
         startTime = time.time()
         if res:
             im_ = f"{count}.png"
-            cv.imwrite(os.path.join('/home/sander/Downloads', im_), frame) 
+            full_path = os.path.join(path,im_)
+            cv.imwrite(full_path, frame)
             #Directory skal være hvor du har projektet gemt
             count += 1
             print("Vi tager et billede")
+            color_matrix = create_matrix(full_path)
 
     cv.imshow("camera", frame)
 
