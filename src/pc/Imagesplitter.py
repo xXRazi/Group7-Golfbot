9c581f0ec1b0006bc15d7f2122ffe50694d48ba5
UnbreakablePotato <swipeytwit@gmail.com>
2026-04-29 11:36:44 +0200

    fixed bug

--- src/pc/Imagesplitter.py
+++ src/pc/Imagesplitter.py
@@ -20,7 +20,7 @@ def create_matrix(img_):
         "B": (np.array([0, 0, 0]), np.array([180, 255, 50])),
         "G": (np.array([40, 50, 50]), np.array([80, 255, 255])),
         "P": (np.array([160, 50, 150]), np.array([180,150,255])),
-        "b": (np.array([85,100,100]), np.array(105,255,255)),
+        "b": (np.array([85,100,100]), np.array([105,255,255])),
         "_": (np.array([115, 120, 120]), np.array([175, 200, 215]))
     }
 
